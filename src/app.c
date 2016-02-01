adf81c04ca917ec5f19c48afb6b5206735610d56
Peter Andersson <pelleplutt1976@gmail.com>
2016-02-01 21:41:19 +0100

    upped generic, mod app acc to new cli struct

--- src/app.c
+++ src/app.c
@@ -15,10 +15,6 @@
 #include "linker_symaccess.h"
 #include <stdarg.h>
 
-#ifdef CONFIG_I2C
-task_mutex i2c_mutex = TASK_MUTEX_INIT;
-#endif
-
 static u8_t cli_buf[16];
 volatile bool cli_rd;
 
@@ -34,7 +30,7 @@ static void cli_task_on_input(u32_t len, void *p) {
 static void cli_rx_avail(u8_t io, void *arg, u16_t available) {
   if (!cli_rd) {
     task *t = TASK_create(cli_task_on_input, 0);
-    TASK_run(t, IO_rx_available(io), (void *)((u32_t)io));
+    TASK_run(t, 0, (void *)((u32_t)io));
     cli_rd = TRUE;
   }
 }
@@ -51,23 +47,12 @@ void APP_shutdown(void) {
 
 void APP_dump(void) {
   print("APP specifics\n-------------\n");
-  print("I2C mutex\n");
-  print("  taken: %s\n", i2c_mutex.taken ? "YES":"NO");
-  if (i2c_mutex.taken) {
-    print("  entries: %i\n", i2c_mutex.entries);
-    print("  owner:   0x%08x  [func:0x%08x]\n", i2c_mutex.owner, i2c_mutex.owner->f);
-    print("  reentr.: %s\n", i2c_mutex.reentrant ? "YES":"NO");
-  }
   print("\n");
 }
 
-CLI_EXTERN_MENU(system)
-CLI_EXTERN_MENU(bus)
-CLI_EXTERN_MENU(dev)
+CLI_EXTERN_MENU(common)
 
 CLI_MENU_START_MAIN
-CLI_SUBMENU(system, "sys", "System submenu")
-CLI_SUBMENU(bus, "bus", "Processor busses submenu")
-CLI_SUBMENU(dev, "dev", "Peripheral devices submenu")
+CLI_EXTRAMENU(common)
 CLI_FUNC("help", cli_help, "Prints help")
 CLI_MENU_END
